3a197e09ab736feddf2aafd3d7ac8be7d5c837ea
Ramadan <jaafariramadan@gmail.com>
2022-04-12 06:28:17 +0300

    working on bugs

--- test.c
+++ test.c
@@ -12,26 +12,30 @@ void newVectorIntTEST()
     VectorInt *v1 = newVectorInt(0);
     VectorInt *v2 = newVectorInt(5);
     VectorInt *v3 = newVectorInt(-5);
-
+/*
     VectorInt *v4 = newFillVectorInt(0, 0);
     VectorInt *v5 = newFillVectorInt(5, 0);
     VectorInt *v6 = newFillVectorInt(5, 10);
     VectorInt *v7 = newFillVectorInt(-2, 4);
-
+    */
     printVectorInt(v1);
     printVectorInt(v2);
     printVectorInt(v3);
+/*
     printVectorInt(v4);
     printVectorInt(v5);
     printVectorInt(v6);
     printVectorInt(v7);
+*/
 
     destroyVectorInt(v1);
     destroyVectorInt(v2);
     destroyVectorInt(v3);
+/*
     destroyVectorInt(v4);
     destroyVectorInt(v5);
     destroyVectorInt(v6);
     destroyVectorInt(v7);
+*/
 }
 #endif
