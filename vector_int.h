41232decd472877e93150d865cd725859582b0c2
Ramadan <jaafariramadan@gmail.com>
2022-04-12 05:37:43 +0300

    added destructor

--- vector_int.h
+++ vector_int.h
@@ -7,12 +7,18 @@
             the capacity of the vector array doubles when its size is equal to it's capacity and an insertion is being done to it,
             and the capacity is halved if the size is half of the capacity and the original capacity is less than the current capacity.
             if the capacity is equal to 0 then it's set to 1 during insertion.
+        Must be Destroyed before the program end using destroyVectorInt(), to not cause memory leaks
 */
 typedef struct VectorInt VectorInt;
 
 /* Constructs an int vector with it's original capacity set to sz
 */
 VectorInt* newVectorInt(unsigned long long sz);
+
+/* Destroys a VectorInt, 
+   Must be called before the program ends, not calling it will cause a memory leak. 
+*/
+void destroyVectorInt(VectorInt* v);
 
 /* Constructs an int vector with original capacity sz, and fill it with value
     if sz is set to 0 the vector is empty.
